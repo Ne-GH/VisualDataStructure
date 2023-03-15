42a2900825e86f3cc8a352dffa69417c00a2da4e
Ne-GH <printfne@gmail.com>
2023-03-16 00:42:19 +0800

    基于数据结构基类重新规划Array

--- Array/Array.hpp
+++ Array/Array.hpp
@@ -8,131 +8,166 @@
 #include "../mainwindow.h"
 #include <vector>
 #include <QPainterPath>
+#include "BaseDataStructure.hpp"
 
-
-template<typename T = int>
-class Array {
-private:
-    class ArrayNode{
-        T val_;
-        int width_{}, height_{};
-        int x_{},y_{};
-    public:
-        explicit ArrayNode(T val);
-        ~ArrayNode() = default;
-        void Draw(QPixmap *pixmap);
-        void SetXY(int x,int y);
-        void SetWH(int w,int h);
-
-    };
-
-private:
-
-    std::vector<ArrayNode> array_{};
-
+template <typename T>
+class Arr : public BaseDataStructure<T> {
+    std::vector<T> _arr;
 public:
-    Array() = default;
-    ~Array() = default;
+    Arr();
+    ~Arr();
+    void Create() override;
+    void Insert(T insert) override;
+    void Delete(T old,T need_delete) override;
+    void Update(T old,T updated) override;
+    T& Search(T search) override;
 
-
-
-
-    void Draw(QWidget *window);
-    void PushBack(T val);
+    void Draw(QPixmap &window) override;
+    void Erase() override;
 };
 
-template<typename T>
-void Array<T>::ArrayNode::SetXY(int x, int y) {
-    x_ = x;
-    y_ = y;
-}
+template <typename T>
+Arr<T>::Arr() {}
+template <typename T>
+Arr<T>::~Arr() {}
+template <typename T>
+void Arr<T>::Create() {}
+template <typename T>
+void Arr<T>::Insert(T insert) {}
+template <typename T>
+void Arr<T>::Delete(T old, T need_delete) {}
+template <typename T>
+void Arr<T>::Update(T old, T updated) {}
+template <typename T>
+T &Arr<T>::Search(T search) {}
+template <typename T>
+void Arr<T>::Draw(QPixmap &window) {}
+template <typename T>
+void Arr<T>::Erase() {}
 
-template<typename T>
-void Array<T>::ArrayNode::SetWH(int w, int h) {
-    width_ = w;
-    height_ = h;
-}
-
-
-/*******************************************************************************
- * ArrayNode 的实现
-*******************************************************************************/
-template<typename T>
-Array<T>::ArrayNode::ArrayNode(T val) :val_(val) {  }
-
-
-template<typename T>
-void Array<T>::ArrayNode::Draw(QPixmap *pixmap) {
-    QPainter pp(pixmap);
-    pp.setRenderHint(QPainter::Antialiasing);
-    pp.setRenderHint(QPainter::SmoothPixmapTransform);
-
-    QRectF rect(x_,y_,width_,height_);
-    pp.drawRect(rect);
-    pp.drawText(rect,Qt::AlignCenter,std::to_string(val_).c_str());
-}
-
-
-
-/*******************************************************************************
- * Array 的实现
- * Array 是ArrayNode的数组形式
-*******************************************************************************/
-
-template<typename T>
-void Array<T>::PushBack(T val) {
-    array_.emplace_back(val);
-}
-/*******************************************************************************
- * 依次调用array中的Draw函数，但是要在之前计算窗口宽高，用来调整数组单个方块的长度
- *
-*******************************************************************************/
-
-template<typename T>
-void Array<T>::Draw(QWidget *window) {
-
-//    int window_width = window->centralWidget()->width();
-//    int window_height = window->centralWidget()->height();
-
-    int window_width = window->width();
-    int window_height = window->height();
-    std::cout << window_width << " " << window_height << std::endl;
-    size_t count_node = array_.size();
-    double available_width = window_width * 0.8;
-    double center_height = window_height*0.4;
-    double single_width = available_width / static_cast<double>(count_node);
-
-    // 将开始绘制的位置定位到窗口的特定位置
-    // width * 0.4?1
-
-//    auto pixmap = new QPixmap(window_width,window_height);
-//    pixmap->fill(Qt::yellow);
+//template<typename T = int>
+//class Array {
+//private:
+//    class ArrayNode{
+//        T val_;
+//        int width_{}, height_{};
+//        int x_{},y_{};
+//    public:
+//        explicit ArrayNode(T val);
+//        ~ArrayNode() = default;
+//        void Draw(QPixmap *pixmap);
+//        void SetXY(int x,int y);
+//        void SetWH(int w,int h);
 //
-//    int x = 0;
+//    };
 //
-//    for(auto node : array_) {
-//        node.SetWH(single_width,single_width);
-//        node.SetXY(x+window_width*0.1,window_height *0.4);
-//        x += static_cast<int>(single_width);
+//private:
 //
-//        node.Draw(pixmap);
-//    }
+//    std::vector<ArrayNode> array_{};
+//
+//public:
+//    Array() = default;
+//    ~Array() = default;
+//
+//
+//
+//
+//    void Draw(QWidget *window);
+//    void PushBack(T val);
+//};
+//
+//template<typename T>
+//void Array<T>::ArrayNode::SetXY(int x, int y) {
+//    x_ = x;
+//    y_ = y;
+//}
+//
+//template<typename T>
+//void Array<T>::ArrayNode::SetWH(int w, int h) {
+//    width_ = w;
+//    height_ = h;
+//}
+//
+//
+///*******************************************************************************
+// * ArrayNode 的实现
+//*******************************************************************************/
+//template<typename T>
+//Array<T>::ArrayNode::ArrayNode(T val) :val_(val) {  }
+//
+//
+//template<typename T>
+//void Array<T>::ArrayNode::Draw(QPixmap *pixmap) {
+//    QPainter pp(pixmap);
+//    pp.setRenderHint(QPainter::Antialiasing);
+//    pp.setRenderHint(QPainter::SmoothPixmapTransform);
+//
+//    QRectF rect(x_,y_,width_,height_);
+//    pp.drawRect(rect);
+//    pp.drawText(rect,Qt::AlignCenter,std::to_string(val_).c_str());
+//}
+//
+//
+//
+///*******************************************************************************
+// * Array 的实现
+// * Array 是ArrayNode的数组形式
+//*******************************************************************************/
+//
+//template<typename T>
+//void Array<T>::PushBack(T val) {
+//    array_.emplace_back(val);
+//}
+///*******************************************************************************
+// * 依次调用array中的Draw函数，但是要在之前计算窗口宽高，用来调整数组单个方块的长度
+// *
+//*******************************************************************************/
+//
+//template<typename T>
+//void Array<T>::Draw(QWidget *window) {
+//
+////    int window_width = window->centralWidget()->width();
+////    int window_height = window->centralWidget()->height();
+//
+//    int window_width = window->width();
+//    int window_height = window->height();
+//    std::cout << window_width << " " << window_height << std::endl;
+//    size_t count_node = array_.size();
+//    double available_width = window_width * 0.8;
+//    double center_height = window_height*0.4;
+//    double single_width = available_width / static_cast<double>(count_node);
+//
+//    // 将开始绘制的位置定位到窗口的特定位置
+//    // width * 0.4?1
+//
+////    auto pixmap = new QPixmap(window_width,window_height);
+////    pixmap->fill(Qt::yellow);
+////
+////    int x = 0;
+////
+////    for(auto node : array_) {
+////        node.SetWH(single_width,single_width);
+////        node.SetXY(x+window_width*0.1,window_height *0.4);
+////        x += static_cast<int>(single_width);
+////
+////        node.Draw(pixmap);
+////    }
+////
+////    QPainter painter(window);
+////    // painter.translate(0,window->menuBar()->height());
+////    painter.drawPixmap(0,0,*pixmap);
+//
 //
 //    QPainter painter(window);
-//    // painter.translate(0,window->menuBar()->height());
+//    auto pixmap = new QPixmap(window_width,window_height);
+//    QPainter pp(pixmap);
+//    pp.drawLine(0,0,100,100);
 //    painter.drawPixmap(0,0,*pixmap);
-
-
-    QPainter painter(window);
-    auto pixmap = new QPixmap(window_width,window_height);
-    QPainter pp(pixmap);
-    pp.drawLine(0,0,100,100);
-    painter.drawPixmap(0,0,*pixmap);
-
-
-
-
-}
-
+//
+//
+//
+//
+//}
+//
 
 #endif //ARRAY_ARRAY_H
--- BaseDataStructure/BaseDataStructure.hpp
+++ BaseDataStructure/BaseDataStructure.hpp
@@ -5,23 +5,31 @@
 #ifndef DATASTRUCTUREVISUALIZATION_BASEDATASTRUCTURE_HPP
 #define DATASTRUCTUREVISUALIZATION_BASEDATASTRUCTURE_HPP
 
-#include <QWidget>
-
+#include <QPixmap>
 template <typename T>
 class BaseDataStructure {
+protected:
+    int _x = 0,_y = 0;
 public:
     BaseDataStructure() = default;
     ~BaseDataStructure() = default;
 
+    virtual void SetDrawPos(int x,int y) final;
     virtual void Create() = 0;
     virtual void Insert(T insert) = 0;
     virtual void Delete(T old,T need_delete) = 0;
     virtual void Update(T old,T updated) = 0;
     virtual T& Search(T search) = 0;
 
-    virtual void Draw(QWidget *window) = 0;
+    virtual void Draw(QPixmap &window) = 0;
     virtual void Erase() = 0;
 };
+
+template <typename T>
+void BaseDataStructure<T>::SetDrawPos(int x, int y) {
+    _x = x;
+    _y = y;
+}
 
 
 #endif //DATASTRUCTUREVISUALIZATION_BASEDATASTRUCTURE_HPP
