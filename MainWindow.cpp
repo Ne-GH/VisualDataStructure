204e013763c48f82e5d6c520e46764d8289a1a8a
Ne-GH <printfne@gmail.com>
2023-07-25 23:49:40 +0800

    排序可以被暂停以及可以选择排序速度

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -6,7 +6,6 @@
 #include <random>
 #include <QThread>
 #include <QPushButton>
-#include <QSlider>
 #include <algorithm>
 #include <QGraphicsView>
 #include <QtCharts/QChart>
@@ -25,62 +24,13 @@
 #include "VisualList.hpp"
 #include "VisualSort.h"
 #include "Array.hpp"
-#include <QThread>
-#include <QSplineSeries>
-#include <QValueAxis>
+
 #include <memory>
 enum class StructType {
     Array,
     Stack,
     Queue
 };
-void CreateSortLayout(QWidget *window,Ui::MainWindow *ui) {
-    QBarSet *set0 = new QBarSet("");
-    QBarSeries *series = new QBarSeries();
-    QChart *chart = new QChart();
-    QBarCategoryAxis *axisX = new QBarCategoryAxis();
-    QValueAxis *axisY = new QValueAxis();
-    QChartView *chartView = new QChartView(chart);
-    QStringList categories;
-    series->append(set0);
-    chart->addSeries(series);
-    chart->setTitle("");
-    // chart->setAnimationOptions(QChart::SeriesAnimations);
-    chart->setAnimationOptions(QChart::NoAnimation);
-//    for (int i = 0;i < visual_sort->GetRandomVector().size(); ++i) {
-//        categories << std::to_string(i).c_str();
-//    }
-    axisX->append(categories);
-    axisX->setVisible(false);
-    axisY->setVisible(false);
-    chart->addAxis(axisX, Qt::AlignBottom);
-    series->attachAxis(axisX);
-    axisY->setRange(0,100);
-    chart->addAxis(axisY, Qt::AlignLeft);
-    series->attachAxis(axisY);
-    chart->legend()->setVisible(false);
-    chart->legend()->setAlignment(Qt::AlignBottom);
-    chartView->setRenderHint(QPainter::Antialiasing);
-//    window->setCentralWidget(chartView);
-
-
-    ui->layout->addWidget(chartView,0,0,10,10);
-
-
-
-
-    QSlider *lider = new QSlider(Qt::Horizontal);
-
-    auto start_but = new QPushButton("开始");
-    auto pause_but = new QPushButton("暂停");
-    auto stop_but = new QPushButton("停止");
-
-    ui->layout->addWidget(start_but, 10, 1, 1, 1);
-    ui->layout->addWidget(pause_but, 10, 3, 1, 1);
-    ui->layout->addWidget(stop_but, 10, 5, 1, 1);
-    ui->layout->addWidget(lider, 10, 7, 1, 2);
-
-}
 
 void CreateMenuAndConnect(MainWindow *window, Ui::MainWindow *ui){
 
@@ -139,98 +89,31 @@ void CreateMenuAndConnect(MainWindow *window, Ui::MainWindow *ui){
     });
 
 
-    auto visual_sort = new VisualSort();
-    visual_sort->GetRandomVector(100);
-    auto run = new QThread();
-
-    QBarSet *set0 = new QBarSet("");
-    QBarSeries *series = new QBarSeries();
-    QChart *chart = new QChart();
-    QBarCategoryAxis *axisX = new QBarCategoryAxis();
-    QValueAxis *axisY = new QValueAxis();
-    QChartView *chartView = new QChartView(chart);
-    auto SetChart = [=]{
-        QStringList categories;
-        series->append(set0);
-        chart->addSeries(series);
-        chart->setTitle("");
-        // chart->setAnimationOptions(QChart::SeriesAnimations);
-        chart->setAnimationOptions(QChart::NoAnimation);
-        for (int i = 0;i < visual_sort->GetRandomVector().size(); ++i) {
-            categories << std::to_string(i).c_str();
-        }
-        axisX->append(categories);
-        axisX->setVisible(false);
-        axisY->setVisible(false);
-        chart->addAxis(axisX, Qt::AlignBottom);
-        series->attachAxis(axisX);
-        axisY->setRange(0,100);
-        chart->addAxis(axisY, Qt::AlignLeft);
-        series->attachAxis(axisY);
-        chart->legend()->setVisible(false);
-        chart->legend()->setAlignment(Qt::AlignBottom);
-        chartView->setRenderHint(QPainter::Antialiasing);
-        window->setCentralWidget(chartView);
-    };
     auto sort_menu = new QMenu("排序");
+    ui->menu_bar->addMenu(sort_menu);
     auto std_sort_action = new QAction("std::sort");
     sort_menu->addAction(std_sort_action);
-    ui->menu_bar->addMenu(sort_menu);
     QObject::connect(std_sort_action,&QAction::triggered,[=]{
-        SetChart();
-        visual_sort->moveToThread(run);
-        QObject::connect(run,&QThread::started,visual_sort,&VisualSort::StdSort);
-        run->start();
-    });
-    auto bubble_sort = new QAction("冒泡排序");
-    sort_menu->addAction(bubble_sort);
-    QObject::connect(bubble_sort,&QAction::triggered,[=]{
-        SetChart();
-        visual_sort->moveToThread(run);
-        QObject::connect(run,&QThread::started,visual_sort,&VisualSort::BubbleSort);
-        run->start();
-    });
-    auto selection_sort = new QAction("选择排序");
-    sort_menu->addAction(selection_sort);
-    QObject::connect(selection_sort,&QAction::triggered,[=]{
-        SetChart();
-        visual_sort->moveToThread(run);
-        QObject::connect(run,&QThread::started,visual_sort,&VisualSort::SelectionSort);
-        run->start();
-    });
-    auto insertion_sort = new QAction("插入排序");
-    sort_menu->addAction(insertion_sort);
-    QObject::connect(insertion_sort,&QAction::triggered,[=]{
-        SetChart();
-        visual_sort->moveToThread(run);
-        QObject::connect(run,&QThread::started,visual_sort,&VisualSort::InsertionSort);
-        run->start();
-    });
-    auto quick_sort = new QAction("快排");
-    sort_menu->addAction(quick_sort);
-    QObject::connect(quick_sort,&QAction::triggered,[=]{
-        SetChart();
-        visual_sort->moveToThread(run);
-        QObject::connect(run,&QThread::started,visual_sort,&VisualSort::QuickSort);
-        run->start();
+        auto visual_sort = new VisualSort();
+        auto sort_thread = new QThread();
+        visual_sort->GetRandomVector(100);
+        visual_sort->CreateSortLayout(ui->layout);
+        visual_sort->moveToThread(sort_thread);
+        QObject::connect(sort_thread,&QThread::started,visual_sort,&VisualSort::StdSort);
+        sort_thread->start();
+
+        QObject::connect(visual_sort,&VisualSort::UPUI,[=]mutable  {
+            auto vec = visual_sort->GetRandomVector();
+            auto set = visual_sort->GetBarSet();
+            set->remove(0,vec.size());
+            for (auto num : vec) {
+                *set << num;
+            }
+        });
     });
 
-    auto merge_sort = new QAction("归并");
-    sort_menu->addAction(merge_sort);
-    QObject::connect(merge_sort,&QAction::triggered,[=]{
-        SetChart();
-        visual_sort->moveToThread(run);
-        QObject::connect(run,&QThread::started,visual_sort,&VisualSort::MergeSort);
-        run->start();
-    });
 
-    QObject::connect(visual_sort,&VisualSort::UPUI,[=]mutable  {
-        auto vec = visual_sort->GetRandomVector();
-        set0->remove(0,vec.size());
-        for (auto num : vec) {
-            *set0 << num;
-        }
-    });
+
 
 
     auto setting_action = new QAction("设置");
@@ -250,7 +133,6 @@ MainWindow::MainWindow(QWidget *parent) :
 
     CreateMenuAndConnect(this,ui);
 
-    CreateSortLayout(this,ui);
 
 
 
--- VisualStructure/VisualSort.h
+++ VisualStructure/VisualSort.h
@@ -13,21 +13,35 @@
 #include <thread>
 #include <algorithm>
 #include <QThread>
-
+#include <QValueAxis>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QChart>
+#include <QThread>
+#include <QSplineSeries>
+#include <QValueAxis>
+#include <QtCharts/QChart>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QChartView>
+#include <QtCharts/QBarSeries>
+#include <QBarCategoryAxis>
 #include "Sort.hpp"
 
 #define CMP \
-    [this](auto val1,auto val2) {\
+    [this](auto val1,auto val2) { \
+        while(_pause);    \
         emit UPUI();\
-        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));\
+        std::this_thread::sleep_for(std::chrono::milliseconds(_sleep_time * (100-_speed) / 100));\
         if (val1 < val2)\
             return true;\
         else\
             return false;\
     }
-#define SORT_FUNC_BASE(func) func(vec.begin(),vec.end(),CMP); \
-                        emit UPUI();\
-                        thread()->quit();
+#define SORT_FUNC_BASE(func) \
+    while(!_start);              \
+    func(vec.begin(),vec.end(),CMP); \
+    emit UPUI();\
+    thread()->quit();
 class VisualSort : public QObject {
     Q_OBJECT
 
@@ -35,8 +49,20 @@ signals:
     void UPUI();
 private:
     std::vector<int >vec;
-    int sleep_time = 10;
+    int _sleep_time = 20;
+    QBarSet *_set = nullptr;
+    QBarSeries *_series = nullptr;
+    QChart *_chart = nullptr;
+    QBarCategoryAxis *_axisX = nullptr;
+    QValueAxis *_axisY = nullptr;
+    QChartView *_chartView = nullptr;
+    // 10 * 50 / 100
+    // 10 * 100 / 100;
 
+    bool _start = false;
+    bool _pause = false;
+    bool _stop = false;
+    int _speed = 50;
 
 public:
 
@@ -72,6 +98,83 @@ public:
     auto GetRandomVector() {
         return vec;
     }
+    QBarSet *GetBarSet() {
+        return _set;
+    }
+
+
+
+
+    void CreateSortLayout(QGridLayout *layout) {
+        _set = new QBarSet("");
+        _series = new QBarSeries();
+        _chart = new QChart();
+        _axisX = new QBarCategoryAxis();
+        _axisY = new QValueAxis();
+        _chartView = new QChartView(_chart);
+        QStringList categories;
+        _series->append(_set);
+        _chart->addSeries(_series);
+        _chart->setTitle("");
+//        _chart->setAnimationOptions(QChart::SeriesAnimations);
+        _chart->setAnimationOptions(QChart::NoAnimation);
+        for (int i = 0;i < GetRandomVector().size(); ++i) {
+            categories << std::to_string(i).c_str();
+        }
+        _axisX->append(categories);
+        _axisX->setVisible(false);
+        _axisY->setVisible(false);
+        _chart->addAxis(_axisX, Qt::AlignBottom);
+        _series->attachAxis(_axisX);
+        _axisY->setRange(0,100);
+        _chart->addAxis(_axisY, Qt::AlignLeft);
+        _series->attachAxis(_axisY);
+        _chart->legend()->setVisible(false);
+        _chart->legend()->setAlignment(Qt::AlignBottom);
+        _chartView->setRenderHint(QPainter::Antialiasing);
+
+        layout->addWidget(_chartView,0,0,10,10);
+
+
+        QSlider *slider = new QSlider(Qt::Horizontal);
+        slider->setValue(50);
+        auto start_but = new QPushButton("开始");
+        auto pause_but = new QPushButton("暂停");
+        auto stop_but = new QPushButton("停止");
+
+        layout->addWidget(start_but, 10, 1, 1, 1);
+        layout->addWidget(pause_but, 10, 3, 1, 1);
+        layout->addWidget(stop_but, 10, 5, 1, 1);
+        layout->addWidget(slider, 10, 7, 1, 2);
+
+        slider->setTickInterval(true);
+        slider->setMaximum(100);
+        QObject::connect(slider,&QSlider::valueChanged,[=]{
+            _speed = slider->value();
+        });
+
+        QObject::connect(start_but,&QPushButton::clicked,[=]{
+            if (_start == false) {
+                _start = true;
+            }
+        });
+        QObject::connect(pause_but,&QPushButton::clicked,[=]{
+            if (_pause == false) {
+                _pause = true;
+                pause_but->setText("继续");
+            }
+            else {
+                _pause = false;
+                pause_but->setText("暂停");
+            }
+        });
+        QObject::connect(stop_but,&QPushButton::clicked,[=]{
+
+        });
+
+
+    }
+
 };
 
 #undef CMP
